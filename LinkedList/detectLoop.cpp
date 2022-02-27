042de4df5e77dd6ef7a452b75d4d21d2e61a158f
zed-shasw <zed.shasw@gmail.com>
2022-02-27 22:19:03 +0530

    up1

---
+++ LinkedList/detectLoop.cpp
@@ -0,0 +1,11 @@
+bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return false;
+        ListNode *slow = head, *fast = head;
+        while(fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
